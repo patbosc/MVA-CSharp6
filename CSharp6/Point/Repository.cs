77a19dc3eeee74aae2f3572a9b837c0d1a8d9b32
Patric Boscolo <patbosc@microsoft.com>
2015-02-05 18:10:12 +0100

    Writing modul 1

---
+++ CSharp6/Point/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+internal class Repository
+{
+    internal static Task<Repository> OpenAsync(string path)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal Task<String> ReadAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal Task LogAsync(RepositoryException e)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal Task CloseAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
